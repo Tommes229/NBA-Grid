43b340eb31cbfcda9c73cefb6eb9fd5458a4262d
Tommes229 <mattis.buehlerjgs@gmail.com>
2023-08-19 22:39:52 +0200

    switch to django
    switch to django from the eel beginnings, first django environment

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# NBAgrid_v1
- 
+# NBAgrid
--- web/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("button").onclick = function() {   
-    eel.one_python()(function(number) {
-        document.querySelector(".one_number").innerHTML = number;
-    })
-}
